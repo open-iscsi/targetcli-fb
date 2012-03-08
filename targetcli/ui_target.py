open-iscsi/targetcli-fb

commit febb269d3af0b2bcf53f19a8a025a2b3df8f5cbf

Show auth status, and errors if auth is misconfigured
For fabrics that support discovery and session auth, indicate if it is
enabled in the summary. This applies to UIFabricModule (for discovery auth
enable and user/pass) as well as for UITPG (session auth enable) and
UINodeACL (session auth user/pass)

Signed-off-by: Andy Grover <agrover@redhat.com>

targetcli/ui_target.py
@@ -149,13 +149,23 @@ class UIFabricModule(UIRTSLibNode):
 
     def summary(self):
         no_targets = len(self._children)
+        status = None
         if self.rtsnode.needs_wwn() and not self.rtsnode.spec['wwn_list']:
             msg = "Not found"
         elif no_targets > 1:
             msg = "%d Targets" % no_targets
         else:
             msg = "%d Target" % no_targets
-        return (msg, None)
+
+        fm = self.rtsnode
+        if fm.has_feature('discovery_auth') and fm.discovery_enable_auth:
+            print fm.discovery_enable_auth
+            msg += ", disc_auth"
+            if not (fm.discovery_password and fm.discovery_userid) and \
+                    not (fm.discovery_mutual_password and fm.discovery_mutual_userid):
+                status = False
+
+        return (msg, status)
 
     def ui_command_create(self, wwn=None):
         '''
@@ -384,13 +394,17 @@ class UITPG(UIRTSLibNode):
             UIPortals(self.rtsnode, self)
 
     def summary(self):
+        status = None
         if self.rtsnode.has_feature('nexus'):
-            description = ("%s" % self.rtsnode.nexus, True)
+            description = str(self.rtsnode.nexus)
         elif self.rtsnode.enable:
-            description = ("enabled", True)
+            description = "enabled"
         else:
-            description = ("disabled", False)
-        return description
+            description, status = ("disabled", False)
+
+        if int(self.rtsnode.get_attribute("authentication")):
+            description += ", auth"
+        return (description, status)
 
     def ui_command_enable(self):
         '''
@@ -609,7 +623,16 @@ class UINodeACL(UIRTSLibNode):
             msg = "%d Mapped LUNs" % no_mluns
         else:
             msg = "%d Mapped LUN" % no_mluns
-        return (msg, None)
+
+        status = None
+        na = self.rtsnode
+        if int(self.parent.parent.rtsnode.get_attribute("authentication")):
+            msg += ", auth"
+            if not (na.chap_password and na.chap_userid) and \
+                    not (na.chap_mutual_password and na.chap_mutual_userid):
+                status = False
+
+        return (msg, status)
 
     def ui_command_create(self, mapped_lun, tpg_lun, write_protect=None):
         '''

commit d6766cc66fed588effd24d4f64ef17e43812f9f7

loop "tpg" has no authentication -> don't show in summary
fix for:
"Cannot find attribute: authentication" at ls command after adding a
loop target.
Problem introduced in febb269d3af0b2bcf53f19a8a025a2b3df8f5cbf

UITPG is inherited by UITarget and that applies to loop targets.
Loop targets have no "authentication" attribute.

Signed-off-by: Johannes Dewender <github@JonnyJD.net>

targetcli/ui_target.py
@@ -406,8 +406,9 @@ class UITPG(UIRTSLibNode):
         else:
             description, status = ("disabled", False)
 
-        if int(self.rtsnode.get_attribute("authentication")):
-            description += ", auth"
+        if "authentication" in self.rtsnode.list_attributes():
+            if int(self.rtsnode.get_attribute("authentication")):
+                description += ", auth"
         return (description, status)
 
     def ui_command_enable(self):