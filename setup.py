999c5675739fbb4b12313a9f32cc708020aabb02
Jerome Martin <jxm@netiant.com>
2014-05-02 16:22:56 +0200

    Updated copyright notices to 2014

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 '''
 This file is part of targetcli.
-Copyright (c) 2011-2013 by Datera, Inc
+Copyright (c) 2011-2014 by Datera, Inc
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may
 not use this file except in compliance with the License. You may obtain
--- targetcli/ui_backstore_legacy.py
+++ targetcli/ui_backstore_legacy.py
@@ -2,7 +2,7 @@
 Implements the targetcli backstores related UI.
 
 his file is part of targetcli.
-Copyright (c) 2011-2013 by Datera, Inc
+Copyright (c) 2011-2014 by Datera, Inc
 
 Licensed under the Apache License, Version 2.0 (the "License"); you may
 not use this file except in compliance with the License. You may obtain
